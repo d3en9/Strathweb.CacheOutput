49611b2144d63bfc69a063a31961457e127d55b5
d3en9 <efmahalin@gmail.com>
2016-09-15 12:10:27 +0300

    add cache attribute for post methods

---
+++ src/WebApi.OutputCache.V2/CacheOutputAllMethodsAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+
+namespace WebApi.OutputCache.V2
+{
+    public class CacheOutputAllMethodsAttribute : CacheOutputAttribute
+    {
+        protected override bool IsCachingAllowed(HttpActionContext actionContext, bool anonymousOnly)
+        {
+            if (anonymousOnly)
+            {
+                if (Thread.CurrentPrincipal.Identity.IsAuthenticated)
+                {
+                    return false;
+                }
+            }
+
+            if (actionContext.ActionDescriptor.GetCustomAttributes<IgnoreCacheOutputAttribute>().Any())
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- src/WebApi.OutputCache.V2/TimeAttributes/CacheOutputUntilToday.cs
+++ src/WebApi.OutputCache.V2/TimeAttributes/CacheOutputUntilToday.cs
@@ -17,4 +17,20 @@ namespace WebApi.OutputCache.V2.TimeAttributes
             CacheTimeQuery = new ThisDay(hour, minute, second);
         }
     }
+
+    public sealed class CacheOutputUntilTodayAllMethods : CacheOutputAllMethodsAttribute
+    {
+        /// <summary>
+        ///     Cache item until absolute expiration today @ 17h45
+        /// </summary>
+        /// <param name="hour">17</param>
+        /// <param name="minute">45</param>
+        /// <param name="second">0</param>
+        public CacheOutputUntilTodayAllMethods(int hour = 23,
+                                     int minute = 59,
+                                     int second = 59)
+        {
+            CacheTimeQuery = new ThisDay(hour, minute, second);
+        }
+    }
 }
